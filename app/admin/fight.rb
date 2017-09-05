36d1b28300d1d633e3c30651afc2685c276ee568
Raghu Betina <raghu@firstdraft.com>
2017-09-05 10:30:41 +0000

    Whitelist columns in Fight for ActiveAdmin

--- app/admin/fight.rb
+++ app/admin/fight.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Fight do
 
+ permit_params :fighter, :fighter2, :venue, :result, :date, :tagline, :attendance, :poster
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
